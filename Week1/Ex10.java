edf9cacf627625ca0235ae3ef766761e92f5f7f3
Steven <s.rekand@khk.ee>
2022-09-06 08:55:32 -0700

    Week1 - Ex 10

---
+++ Week1/Ex10.java
@@ -0,0 +1,10 @@
+package Week1;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        int radius = 20;
+        double result = radius * 2 * Math.PI;
+
+        System.out.println(result);
+    }
+}
