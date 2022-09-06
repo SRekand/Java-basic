9bf25382074c948c04c032b2d6c8fbbda0d7ec33
Steven <s.rekand@khk.ee>
2022-09-06 08:50:24 -0700

    Week1 - Ex 9

---
+++ Week1/Ex9.java
@@ -0,0 +1,12 @@
+package Week1;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        int first = 3;
+        int second = 2;
+
+        double result = (double)first / second;
+
+        System.out.println(result);
+    }
+}
