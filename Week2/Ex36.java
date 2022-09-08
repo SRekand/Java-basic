b2efca071cf843f3799528cc02e2b1a411c76389
Steven <s.rekand@khk.ee>
2022-09-08 02:38:11 -0700

    Week2 - Ex 36.5: Even and odd numbers

--- Week2/Ex36.java
+++ Week2/Ex36.java
@@ -10,7 +10,8 @@ public class Ex36 {
         int number;
         int sum = 0;
         int count = 0;
-
+        int even = 0;
+        int odd = 0;
 
         while(true) {
             number = Integer.parseInt(reader.nextLine());
@@ -19,10 +20,17 @@ public class Ex36 {
             }
             sum += number;
             count++;
+            if (number % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
         }
         System.out.println("Thank you and see you later!");
         System.out.println("Sum is " + sum);
         System.out.println("How many number: " + count);
         System.out.println("Average: " + (double) sum / count);
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
     }
 }
