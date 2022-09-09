804791acf846812a2044f3f1bb5be2b5855d0510
Steven <s.rekand@khk.ee>
2022-09-08 23:14:58 -0700

    Week2 - Ex 72.2: Your first money transfer

--- Week2/Ex39.java
+++ Week2/Ex39.java
@@ -4,15 +4,26 @@ public class Ex39 {
     private static void printStars(int amount) {
         int times = 1;
 
-        while (true) {
+        while (times <= amount) {
             System.out.print("*");
-            if (times == amount){
-                break;
-            }
             times++;
         }
     }
+
+    private static void printSquare(int sideSize) {
+        int row = 1;
+        while (row <= sideSize) {
+            int field = 1;
+            while (field <= sideSize) {
+                System.out.println("*");
+                field++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
     public static void main(String[] args) {
-        printStars(2);
+        printSquare(4);
     }
 }
