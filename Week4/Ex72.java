66357c99663b51c237c5994d0018a001eee706f4
Steven <s.rekand@khk.ee>
2022-09-08 23:24:27 -0700

    Week2 - Ex 72.3: Money transfers

--- Week4/Ex72.java
+++ Week4/Ex72.java
@@ -2,15 +2,25 @@ package Week4;
 
 public class Ex72 {
     public static void main(String[] args) {
-        Account mattAccount = new Account("Matt's account", 1000);
-        Account myAccount = new Account("My account", 0);
+        Account A = new Account("A account", 100);
+        Account B = new Account("B account", 0);
+        Account C = new Account("C account", 0);
 
-        System.out.println(mattAccount);
-        System.out.println(myAccount);
+        transfer(A, B,50);
 
-        mattAccount.withdrawal(100);
-        myAccount.deposit(100);
-        System.out.println(mattAccount);
-        System.out.println(myAccount);
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+
+        transfer(B, C,25);
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    }
+
+    public static void transfer(Account from, Account to, double amount) {
+        from.withdrawal(amount);
+        to.deposit(amount);
     }
 }
