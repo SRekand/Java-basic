a0acdb6cf90b3ffa4e3c009188d3f75cd1b7a7fa
Steven <s.rekand@khk.ee>
2022-12-19 03:06:10 -0800

    Week5 done!

---
+++ Week5/Ex86.java
@@ -0,0 +1,16 @@
+package Week5;
+
+public class Ex86 {
+    public static void main(String[] args) {
+        LyyraCard cardOfPekka = new LyyraCard(10);
+
+        System.out.println("money on the card " + cardOfPekka.balance() );
+        boolean succeeded = cardOfPekka.pay(8);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
+
+        succeeded = cardOfPekka.pay(4);
+        System.out.println("money taken: " + succeeded );
+        System.out.println("money on the card " + cardOfPekka.balance() );
+    }
+}
