aa36f2e24d344a74c109f1cb9d71b275101c83ff
Steven <s.rekand@khk.ee>
2022-12-19 03:19:36 -0800

    Week6 done!

---
+++ Week6/Ex98.java
@@ -0,0 +1,25 @@
+package Week6;
+
+import java.util.Arrays;
+
+public class Ex98 {
+    public static void main(String[] args) {
+        int[] original = {1, 2, 3, 4};
+        int[] copied = copy(original);
+
+        // change the copied
+        copied[0] = 99;
+
+        // print both
+        System.out.println( "original: " + Arrays.toString(original));
+        System.out.println( "copied: " + Arrays.toString(copied));
+    }
+
+    public static int[] copy(int[] array) {
+        int[] copy = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copy[i] = array[i];
+        }
+        return copy;
+    }
+}
